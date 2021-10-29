76320e8b490950294cfae2ebdbe75fec099c676a
Thiago <74683757+ThiagoShow@users.noreply.github.com>
2021-10-29 13:04:02 +0100

    psi 29/10/2021
    psi 29/10/2021

---
+++ Pitag1/frmPitagora.vb
@@ -0,0 +1,15 @@
+﻿Public Class frmPitagora
+    Private Sub Button1_Click(sender As Object, e As EventArgs)
+        If String.IsNullOrEmpty(txtA.Text) And String.IsNullOrEmpty(txtB.Text) And String.IsNullOrEmpty(txtH.Text) Then
+            MessageBox.Show("Informe pelo menos dois valores para calcular...")
+        ElseIf Not String.IsNullOrEmpty(txtA.Text) And Not String.IsNullOrEmpty(txtB.Text) Then
+            Dim a As Single = Convert.ToSingle(txtA.Text)
+            Dim b As Single = Convert.ToSingle(txtB.Text)
+        End If
+    End Sub
+
+    Private Sub frmPitagora_Load(sender As Object, e As EventArgs)
+
+    End Sub
+
+End Class
