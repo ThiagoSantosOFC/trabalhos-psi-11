04e7193f50597e7e025fa52bc784027afc48a29f
Thiago <74683757+ThiagoShow@users.noreply.github.com>
2022-02-17 17:06:02 +0000

    aaa

---
+++ Trabalho Final mod 11/frmDHT.Designer.vb
@@ -0,0 +1,37 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class frmDHT
+    Inherits System.Windows.Forms.Form
+
+    'Descartar substituições de formulário para limpar a lista de componentes.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Exigido pelo Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'OBSERVAÇÃO: o procedimento a seguir é exigido pelo Windows Form Designer
+    'Pode ser modificado usando o Windows Form Designer.  
+    'Não o modifique usando o editor de códigos.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.SuspendLayout()
+        '
+        'frmDHT
+        '
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.None
+        Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.Name = "frmDHT"
+        Me.Text = "MultiMeter"
+        Me.ResumeLayout(False)
+
+    End Sub
+
+End Class
---
+++ Trabalho Final mod 11/frmDHT.vb
@@ -0,0 +1,15 @@
+﻿Imports System.IO
+Imports 
+Public Class frmDHT
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+    End Sub
+End Class
+
+Public Class Dados
+    Dim int_IDleitura As Integer
+    Dim data_Reg As Date
+    Dim temp As Double
+    Dim Hum As Double
+    Dim flag As Boolean
+End Class
