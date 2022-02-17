073a96f5fd22084bff08dea479dd42f3d70ac60b
Thiago <74683757+ThiagoShow@users.noreply.github.com>
2022-02-17 17:39:11 +0000

    aaa

--- Trabalho Final mod 11/frmDHT.Designer.vb
+++ Trabalho Final mod 11/frmDHT.Designer.vb
@@ -22,16 +22,36 @@ Partial Class frmDHT
     'Não o modifique usando o editor de códigos.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Me.SerialPort1 = New System.IO.Ports.SerialPort(Me.components)
+        Me.DataGridView1 = New System.Windows.Forms.DataGridView()
+        CType(Me.DataGridView1, System.ComponentModel.ISupportInitialize).BeginInit()
         Me.SuspendLayout()
+        '
+        'SerialPort1
+        '
+        Me.SerialPort1.PortName = "COM6"
+        '
+        'DataGridView1
+        '
+        Me.DataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
+        Me.DataGridView1.Location = New System.Drawing.Point(12, 12)
+        Me.DataGridView1.Name = "DataGridView1"
+        Me.DataGridView1.Size = New System.Drawing.Size(776, 397)
+        Me.DataGridView1.TabIndex = 0
         '
         'frmDHT
         '
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.None
         Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.Controls.Add(Me.DataGridView1)
         Me.Name = "frmDHT"
         Me.Text = "MultiMeter"
+        CType(Me.DataGridView1, System.ComponentModel.ISupportInitialize).EndInit()
         Me.ResumeLayout(False)
 
     End Sub
 
+    Friend WithEvents SerialPort1 As IO.Ports.SerialPort
+    Friend WithEvents DataGridView1 As DataGridView
 End Class
--- Trabalho Final mod 11/frmDHT.vb
+++ Trabalho Final mod 11/frmDHT.vb
@@ -1,14 +1,35 @@
 ﻿Imports System.IO
-Imports 
+Imports System.IO.Ports
 Public Class frmDHT
-    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+    Dim lista_dados As New ArrayList
+    Sub frmDHT_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Dim f1 As FileStream = New FileStream("dados.bin", FileMode.OpenOrCreate, FileAccess.ReadWrite)
+        Dim ReaderF1 As New BinaryReader(f1)
+
+        If (f1.Length = 0) Then
+            MsgBox("Ficheiro vazio, sem produtos registrados")
+        Else
+            Do While ReaderF1.PeekChar <> -1
+                Dim novo As New Dados
+                novo.int_IDleitura = ReaderF1.ReadInt32()
+                novo.data_Reg = ReaderF1.ReadString()
+                novo.temp = ReaderF1.ReadDouble()
+                novo.Hum = ReaderF1.ReadDouble()
+            Loop
+        End If
+        ReaderF1.Close()
+        f1.Close()
+    End Sub
+
+    Private Sub DataGridView1_CellContentClick(sender As Object, e As DataGridViewCellEventArgs) Handles DataGridView1.CellContentClick
 
     End Sub
 End Class
 
 Public Class Dados
     Dim int_IDleitura As Integer
-    Dim data_Reg As Date
+    Dim data_Reg As Date = Date.Now
     Dim temp As Double
     Dim Hum As Double
     Dim flag As Boolean
