10c7fdaed8fc39aab10f8939e34c5a222d1944a5
Thiago <74683757+ThiagoShow@users.noreply.github.com>
2022-02-24 23:54:09 +0000

    Update dht11.ino

--- dht11/dht11.ino
+++ dht11/dht11.ino
@@ -1,16 +1,14 @@
-// Example testing sketch for various DHT humidity/temperature sensors
-// Written by ladyada, public domain
+
 
 #include "DHT.h"
 
-#define DHTPIN 13     // what pin we're connected to
+#define DHTPIN 13     
 
-// Uncomment whatever type you're using!
+
 #define DHTTYPE DHT11   // DHT 11 
-//#define DHTTYPE DHT22   // DHT 22  (AM2302)
-//#define DHTTYPE DHT21   // DHT 21 (AM2301)
 
-// Initialize DHT sensor for normal 16mhz Arduino
+
+
 DHT dht(DHTPIN, DHTTYPE);
 
 void setup() {
@@ -24,22 +22,18 @@ void loop() {
   // Wait a few seconds between measurements.
   delay(5000);
 
-  // Reading temperature or humidity takes about 250 milliseconds!
-  // Sensor readings may also be up to 2 seconds 'old' (its a very slow sensor)
   float h = dht.readHumidity();
-  // Read temperature as Celsius
+
   float t = dht.readTemperature();
-  // Read temperature as Fahrenheit
+
   float f = dht.readTemperature(true);
   
-  // Check if any reads failed and exit early (to try again).
+
   if (isnan(h) || isnan(t) || isnan(f)) {
     Serial.println("Failed to read from DHT sensor!");
     return;
   }
 
-  // Compute heat index
-  // Must send in temp in Fahrenheit!
   float hi = dht.computeHeatIndex(f, h);
 
   Serial.print("Humidity: "); 
